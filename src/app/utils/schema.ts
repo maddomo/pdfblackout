9a5cb085ee4719196bd1c51cf1c1bef45cc61bd3
maddomo <moritz-10@web.de>
2025-03-04 16:28:07 +0100

    added schemas

---
+++ src/app/utils/schema.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+const nameRegex = /^[A-Za-z]+(?:[-'][A-Za-z]+)*$/g;
+
+export const signupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(1),
+    firstName: z
+      .string()
+      .regex(nameRegex, "First name cant have special characters or spaces"),
+    lastName: z.string().regex(nameRegex),
+  });
+
+export const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(1),
+})
+
+export const signupCompleteSchema = z.object({
+    firstName: z.string().regex(nameRegex),
+    lastName: z.string().regex(nameRegex),
+  });
