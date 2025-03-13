f409f68f778fee9dde03e2155bd3f9fe9cfccef3
Moritz <moritz.foglia@mni.thm.de>
2025-03-13 14:41:24 +0100

    type

--- src/app/api/deletePDFCron/route.ts
+++ src/app/api/deletePDFCron/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { db } from "~/server/db"; // Dein Prisma Client
 import { createClient } from "@supabase/supabase-js";
 
